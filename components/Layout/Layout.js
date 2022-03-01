1762562a524668456d176893fb51fdcbf5973986
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-01 20:29:52 +0100

    added custom favicon

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,8 +1,12 @@
+import Head from "next/head";
 import Navigate from "../Navigate/Navigate";
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.png" />
+      </Head>
       <Navigate />
       {children}
     </>
