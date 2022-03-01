196f9ab0f20568d8bcc9bdc0a62f11f7bca7a118
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-01 19:14:04 +0100

    fixed heroku urls

--- pages/mypokemon-isr/index.js
+++ pages/mypokemon-isr/index.js
@@ -14,9 +14,7 @@ const MyPokemonISR = ({ pokemon }) => {
 };
 
 export const getStaticProps = async () => {
-  const response = await fetch(
-    "https://w8-poke-api.herokuapp.com/pokemon/pokemon"
-  );
+  const response = await fetch("https://w8-poke-api.herokuapp.com/pokemon");
 
   const pokemon = await response.json();
 
--- pages/mypokemon-ssr/index.js
+++ pages/mypokemon-ssr/index.js
@@ -14,9 +14,7 @@ const MyPokemonSSR = ({ pokemon }) => {
 };
 
 export const getServerSideProps = async () => {
-  const response = await fetch(
-    "https://w8-poke-api.herokuapp.com/pokemon/pokemon"
-  );
+  const response = await fetch("https://w8-poke-api.herokuapp.com/pokemon");
 
   const pokemon = await response.json();
 
