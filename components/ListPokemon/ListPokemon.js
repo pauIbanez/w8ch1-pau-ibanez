9e57a6ca298896fef11817ee044842b6c68e560c
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-01 16:57:59 +0100

    added pokemon data

--- components/ListPokemon/ListPokemon.js
+++ components/ListPokemon/ListPokemon.js
@@ -1,7 +1,41 @@
+/* eslint-disable @next/next/no-img-element */
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
 const ListPokemon = ({ pokemon }) => {
+  const [pokemonData, setPokemonData] = useState({});
+
+  useEffect(() =>
+    (async () => {
+      const response = await fetch(pokemon.url);
+      const body = await response.json();
+      setPokemonData(body);
+    })()
+  );
+
+  let types = [];
+
+  if (pokemonData.types) {
+    types = pokemonData.types.map(({ type: { name } }) => (
+      <p key={name}>{name}</p>
+    ));
+  }
+
   return (
     <li>
       <h2>{pokemon.name}</h2>
+      {pokemonData.name && (
+        <>
+          <h3>Types:</h3>
+          {types}
+          <img
+            src={pokemonData.sprites.other["official-artwork"].front_default}
+            alt={`${pokemonData.name} picture`}
+            width="100"
+            height="100"
+          />
+        </>
+      )}
     </li>
   );
 };
