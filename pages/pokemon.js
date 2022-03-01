a55f13615130b32b4aa0ec891793450978126349
Pau Ibáñez <pauibanez2001@gmail.com>
2022-03-01 17:18:00 +0100

    fixed infinite loop

--- pages/pokemon.js
+++ pages/pokemon.js
@@ -4,18 +4,20 @@ import ListPokemon from "../components/ListPokemon/ListPokemon";
 const Pokemon = () => {
   const [pokemonsToRender, setPokemonsToRender] = useState([]);
 
-  useEffect(() =>
-    (async () => {
-      const response = await fetch(
-        "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0"
-      );
+  useEffect(
+    () =>
+      (async () => {
+        const response = await fetch(
+          "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0"
+        );
 
-      const { results: pokemon } = await response.json();
+        const { results: pokemon } = await response.json();
 
-      setPokemonsToRender(
-        pokemon.map((poke) => <ListPokemon key={poke.name} pokemon={poke} />)
-      );
-    })()
+        setPokemonsToRender(
+          pokemon.map((poke) => <ListPokemon key={poke.name} pokemon={poke} />)
+        );
+      })(),
+    []
   );
   return (
     <>
